f5940be1942fbaf400991c816ffc2f5a37887d23
NullString <53035336+NullString1@users.noreply.github.com>
2021-10-04 22:57:40 +0100

    Update game.py

--- game.py
+++ game.py
@@ -41,15 +41,18 @@ class eventHandler():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                 [btn.onClick() for btn in btns if btn.rect.collidepoint(pygame.mouse.get_pos())]
+                     
         self.chtr.draw()
-
-class entity():
-    def __init__(self, surface, imgpath):
-        self.img = pygame.image.load(imgpath)
+        [btn.draw() for btn in btns]
+class gobject():
+    def __init__(self, surface):
+        #self.img = pygame.image.load(imgpath)
         #self.img = pygame.transform.scale(self.img, (50, 80))
-        self.imgfile = imgpath
+        #self.imgfile = imgpath
         self.surface = surface
-        #self.pos = coords(self.surface.get_size())/2
+        self.pos = coords(self.surface.get_size())/2
 
     def setposr(self, xy): #Move relative
         self.pos += coords(xy)
@@ -60,7 +63,15 @@ class entity():
     def draw(self):
         self.surface.fill((255,255,255))
         self.surface.blit(self.img, self.pos)
-        pygame.display.update()
+        #pygame.display.update()
+
+
+class entity(gobject):
+    def __init__(self, surface, imgpath):
+        gobject.__init__(self, surface)
+        self.img = pygame.image.load(imgpath)
+        self.img = pygame.transform.scale(self.img, (50, 80))
+        self.imgfile = imgpath
 
     def move(self, xy):
         final=self.pos+xy
@@ -85,12 +96,6 @@ class entity():
             self.draw()
             clock.tick()
 
-
-
-    imgfile = ""
-    img = None
-    score = 0
-
 class chtr(entity):
     def __init__(self, surface, imgpath):
         entity.__init__(self, surface, imgpath)
@@ -104,25 +109,47 @@ class chtr(entity):
     def down(self):
         self.move(coords(0, self.speed))
 
+class button(gobject):
+    def __init__(self, surface, size):
+        gobject.__init__(self, surface)
+        self.bsurface = pygame.Surface(size)
+        self.bsurface.fill([0,0,0])
+        self.rect = pygame.Rect(self.pos[0], self.pos[1], size[0], size[1])
+        self.surface.blit(self.bsurface, self.pos)
 
+    def draw(self):
+        self.surface.blit(self.bsurface, self.pos)
+
+
+    def onClick(self):
+        print("Clicked")
+
+def showMenu(surface):
+    font = pygame.font.Font("Arial.ttf", 30)
+    title = font.render("Duck Game", True, [255,255,255])
+    
 clock = pygame.time.Clock()
+btns = []
 def main():
 
     pygame.init()
 
     surface = pygame.display.set_mode((1280,720))
-
+    pygame.display.set_caption("Game")
+    
     pygame.draw.rect(surface, (255,255,255), [0,0, *surface.get_size()])
 
     duck = chtr(surface, "duck.png")
     eHandler = eventHandler(duck, surface)
 
     duck.draw()
-
+    
     playing = True
 
+    btns.append(button(surface, (200,200)))
     while playing:
         eHandler.hEvents()
+        pygame.display.update()
         duck.move(coords(0, 3))
         duck.move(coords(10, 0))
         clock.tick(60)
